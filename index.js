adc1765b68cfa625fa7dab91479867de58840b96
Gilda A <gilda.ahmadniaye@gmail.com>
2021-05-12 15:22:56 +0200

    Implemented a ground that runs and a cat that can jump and swim and run

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var config = {
   type: Phaser.AUTO,
-  width: 800,
-  height: 600,
+  width: 1000,
+  height: 400,
+  transparent: true,
   physics: {
     default: 'arcade',
     arcade: {
-      gravity: { y: 300 },
       debug: false,
     },
   },
@@ -19,80 +19,50 @@ var config = {
 var game = new Phaser.Game(config);
 
 function preload() {
-  this.load.image('ocean', 'assets/ocean.png');
-  this.load.image('ground', 'assets/platform.png');
-  this.load.image('octopus', 'assets/octopus.png');
-  this.load.spritesheet('cat', 'assets/CATSUNICORN.png', {
-    frameWidth: 105,
-    frameHeight: 143,
+  this.load.image('background', 'assets/ocean.png');
+  this.load.image('ground', 'assets/ground.png');
+  this.load.image('cat-idle', 'assets/cat-idle.png');
+
+  this.load.spritesheet('cat', 'assets/cat-run.png', {
+    frameWidth: 84,
+    frameHeight: 94,
   });
-  // this.load.image('cat', 'assets/cat.png');
-  // { frameWidth: 32, frameHeight: 48 };
 }
 
 function create() {
-  //this.gameSpeed = 10;
+  this.add.image(0, 0, 'background').setOrigin(0, 0);
 
-  this.ground = this.add.tileSprite(0, 600, 800, 32, 'ground').setOrigin(0, 1);
+  this.gameSpeed = 5;
+  const { height, width } = this.game.config;
 
-  //this.add.image(400, 300, 'ocean');
+  this.ground = this.add
+    .tileSprite(0, height, width, 28, 'ground')
+    .setOrigin(0, 1);
 
-  // platforms = this.physics.add.staticGroup();
-
-  // platforms.create(400, 568, 'ground').setScale(2).refreshBody();
-
-  //this.add.image();
-
-  player = this.physics.add.sprite(100, 450, 'cat');
-
-  player.setBounce(0.2);
-  player.setCollideWorldBounds(true);
+  this.cat = this.physics.add
+    .sprite(0, height, 'cat-idle')
+    .setOrigin(0, 1)
+    .setCollideWorldBounds(true)
+    .setGravityY(600);
 
   this.anims.create({
-    key: 'left',
-    frames: this.anims.generateFrameNumbers('cat', { start: 0, end: 3 }),
+    key: 'cat-run',
+    frames: this.anims.generateFrameNumbers('cat', { start: 2, end: 3 }),
     frameRate: 10,
     repeat: -1,
   });
 
-  this.anims.create({
-    key: 'turn',
-    frames: [{ key: 'cat', frame: 4 }],
-    frameRate: 20,
-  });
-
-  this.anims.create({
-    key: 'right',
-    frames: this.anims.generateFrameNumbers('cat', { start: 5, end: 8 }),
-    frameRate: 10,
-    repeat: -1,
-  });
-
-  cursors = this.input.keyboard.createCursorKeys();
-
-  this.physics.add.collider(player, platforms);
+  this.spacebar = this.input.keyboard.addKey(
+    Phaser.Input.Keyboard.KeyCodes.SPACE
+  );
 }
 
 function update() {
-  //this.ground.tilePositionX += 10;
+  this.ground.tilePositionX += this.gameSpeed;
 
-  this.ground.autoScroll(-150, 0);
+  if (Phaser.Input.Keyboard.JustDown(this.spacebar)) {
+    this.cat.setVelocityY(-300);
+  }
 
-  // if (cursors.left.isDown) {
-  //   player.setVelocityX(-160);
-
-  //   player.anims.play('left', true);
-  // } else if (cursors.right.isDown) {
-  //   player.setVelocityX(160);
-
-  //   player.anims.play('right', true);
-  // } else {
-  //   player.setVelocityX(0);
-
-  //   player.anims.play('turn');
-  // }
-
-  // if (cursors.up.isDown && player.body.touching.down) {
-  //   player.setVelocityY(-300);
-  // }
+  this.cat.play('cat-run', true);
 }
