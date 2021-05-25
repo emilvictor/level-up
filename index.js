42db46394f4c18876d285886212ea37c64103583
Gilda A <gilda.ahmadniaye@gmail.com>
2021-05-25 12:51:20 +0200

    last touchups and cleaned the code

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ var config = {
   },
 };
 
-var game = new Phaser.Game(config);
+new Phaser.Game(config);
 
 function preload() {
   this.load.image('background', '/ocean.png');
@@ -26,7 +26,6 @@ function preload() {
   this.load.image('obsticle', '/octopus.png');
   this.load.image('obsticle2', '/coral.png');
   this.load.image('obsticle3', '/shark.png');
-  this.load.image('replayButton', '/replay.png');
 
   this.load.spritesheet('cat', '/cat-run2.png', {
     frameWidth: 74,
@@ -37,18 +36,19 @@ function preload() {
 var text;
 var score = 0;
 var timedEvent;
-var resetEvent;
 
 function create() {
   this.add.image(0, 0, 'background').setOrigin(0, 0);
 
+  //groundspeed
   this.gameSpeed = 5;
   const { height, width } = this.game.config;
-
+  //ground
   this.ground = this.add
     .tileSprite(0, height, width, 28, 'ground')
     .setOrigin(0, 1);
 
+  //obsticles
   this.obsticle = this.physics.add.sprite(width + 300, height / 2, 'obsticle');
   this.obsticle2 = this.physics.add.sprite(width, height - 40, 'obsticle2');
   this.obsticle3 = this.physics.add.sprite(
@@ -57,12 +57,13 @@ function create() {
     'obsticle3'
   );
 
+  //cat
   this.cat = this.physics.add
     .sprite(0, height, 'cat-idle')
     .setOrigin(-0.4, 1)
     .setCollideWorldBounds(true)
     .setGravityY(600);
-
+  //cat run
   this.anims.create({
     key: 'cat-run',
     frames: this.anims.generateFrameNumbers('cat', { start: 2, end: 3 }),
@@ -70,19 +71,14 @@ function create() {
     repeat: -1,
   });
 
+  //spacebar jump
   this.spacebar = this.input.keyboard.addKey(
     Phaser.Input.Keyboard.KeyCodes.SPACE
   );
 
-  ///////////// click test ///////////
-  this.input.on(Phaser.Input.Events.POINTER_DOWN, function (pointer) {
-    console.log('click');
-  });
-  ////////===///////====///////
-
-  // score
+  // scoretext
   text = this.add.text(16, 16, '0', { fontSize: '22px', fill: '#000' });
-
+  //score counting
   timedEvent = this.time.addEvent({
     delay: 200,
     callback: () => {
@@ -102,29 +98,17 @@ function create() {
 function gameOver() {
   console.log('GAME OVER!');
   this.scene.pause();
-  this.add.text(340, 150, 'GAME OVER', {
-    fontSize: '62px',
+  this.add.text(310, 170, 'GAME OVER', {
+    fontSize: 'bold 66px',
     fill: '#FF0000',
+    stroke: '#000',
+    strokeThickness: 4,
   });
-
-  // this.restart = this.add
-  //   .image(500, 250, 'replayButton')
-  //   .setInteractive()
-  //   .on(Phaser.Input.Events.POINTER_DOWN, function (pointer) {
-  //     window.location.reload('Refresh');
-  //   });
-
-  // reloade sidan med click?
-  // this.input.on(Phaser.Input.Events.POINTER_DOWN, function (pointer) {
-  //   window.location.reload('Refresh');
-  // });
-
-  //reloada sidan:
-  // window.location.reload('Refresh');
 }
 
 // ============== OBSTICLES ============== //
 //Move obsticle towards cat and update on random y-location
+//obsticle octopus
 function moveObsticle(obsticle, speed) {
   obsticle.x -= speed;
   if (obsticle.x < 0) {
@@ -133,14 +117,15 @@ function moveObsticle(obsticle, speed) {
     obsticle.y = randomY;
   }
 }
-
+//obsticle plant
 function moveObsticle2(obsticle2, speed) {
   obsticle2.x -= speed;
   if (obsticle2.x < 0) {
     obsticle2.x = 1100;
   }
 }
-
+//Move obsticle towards cat and update on random y-location
+//obsticle shark
 function moveObsticle3(obsticle3, speed) {
   obsticle3.x -= speed;
   if (obsticle3.x < 0) {
@@ -151,17 +136,22 @@ function moveObsticle3(obsticle3, speed) {
 }
 
 function update() {
+  //groundspeed
   this.ground.tilePositionX += this.gameSpeed;
 
+  //obsticlespeed
   moveObsticle(this.obsticle, 6);
   moveObsticle2(this.obsticle2, 5);
   moveObsticle3(this.obsticle3, 7);
 
+  //gravity jump
   if (Phaser.Input.Keyboard.JustDown(this.spacebar)) {
     this.cat.setVelocityY(-300);
   }
 
+  //cat run
   this.cat.play('cat-run', true);
 
+  //scoretext
   text.setText('Score: ' + score);
 }
